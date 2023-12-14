ed90ef34cfdcea12f39c6536c6875e1eb2f76e69
Marco-Antonio-Ibarra <marcoantonioibarra03@iesamachado.org>
2023-12-14 23:18:05 +0100

    javascript

---
+++ Workspace/Javascript carpeta/codigo.js
@@ -0,0 +1,29 @@
+/* Todo lo que lleva un nombre y un igual (por ejemplo: recipiente = ("papel") es una variable que contiene un dato. En este caso, la variable es "recipiente" y el dato es "papel")*/
+
+
+/* String = "cadena de texto",  esto es una cadena de texto, donde cada
+    letra (incluso los espacios) es importante, ya que son caracteres unidos */
+
+/* Number = 19, esto es un número, por lo que no va entre comillas. Si fuera entre comillas, sería una cadena de texto*/
+
+/* Boolean = true, se traduce como 0 o 1, sin embargo se coloca como true/false */
+
+/* Hay 3 formas de declarar una variable. Si declaro una variable, todo lo que este dentro de un bloque va a tomar esa variable, por fuera no */
+
+/* VarDate, let, const */
+
+/* var numero = 15,  Var se trata de alcance global */
+
+/* let = , tiene menos alcance que var. Mediante let se declara una variable, aunque no es necesario darle un valor, ya que solo se busca conocer el alcance */
+
+/* const nombre = "dalto",  Const es una constante y las variables constantes no pueden cambiar su valor (una vez definidas no se pueden volver a definir, no tienen cambio), en este caso dalto siempre va a ser el nombre */
+
+let numero, numero2, numero3;
+
+numero = 1;
+numero2 = 9;
+numero3 = 4;
+
+alert(numero); /* PD: "Undefined es un tipo de dato que nos dice que la variable existe pero no tiene valor (está sin definir)*/
+alert(numero2);
+alert(numero3);
