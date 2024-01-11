278847c7fa9359e2b5d3e508770a05308fe4a7a8
Marco-Ibarra-Gomez <marcoantonioibarra03@iesamachado.org>
2024-01-11 13:42:41 +0100

    11/1/2024

---
+++ Tema 3/javascript ficheros/círculos.js
@@ -0,0 +1,19 @@
+function ocultarrojo(){
+    document.getElementById('circulorojo').style.visibility='hidden';
+}
+function ocultarverde(){
+    document.getElementById('circuloverde').style.visibility='hidden';
+}
+function ocultarazul(){
+    document.getElementById('circuloazul').style.visibility='hidden';
+}
+
+function mostrarrojo(){
+    document.getElementById('circulorojo').style.visibility='visible';
+}
+function mostrarverde(){
+    document.getElementById('circuloverde').style.visibility='visible';
+}
+function mostrarazul(){
+    document.getElementById('circuloazul').style.visibility='visible';
+}
